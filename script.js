7f9c6b2aff874b3da0bd906be84eaae7862fb0b2
Fabienne <fortunio.fabienne@gmail.com>
2019-12-03 20:06:01 +0100

    milestone 1 done

--- script.js
+++ script.js
@@ -20,5 +20,22 @@ $(document).ready(function() {
     }
  });
     // intercetto il tasto INVIO
-    
+    $('.msg-bottom').keypress(function(event) {
+        if(event.which == 13) {
+            var testo_messaggio = $('.msg-bottom').val();
+            if(testo_messaggio.length != 0) {
+                // clono il template del messaggio
+                var nuovo_messaggio = $('.template .message').clone();
+                // inserisco nel giusto span il testo del messaggio
+                nuovo_messaggio.children('.message-text').text(testo_messaggio);
+                // aggiungo al fiv .message la classe sent
+                nuovo_messaggio.addClass('sent');
+                // inserisco il messaggio all'interno del container
+                $('.right-messages.active').append(nuovo_messaggio);
+                //resetto l'imput con una stringa vuota
+                $('.msg-bottom').val('');
+
+        }
+        }
+    });
 });
